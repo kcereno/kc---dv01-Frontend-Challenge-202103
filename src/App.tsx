a00ebf5f74f34fd388150bd415b5293c1a1b905c
Karl <karlcereno@gmail.com>
2024-02-06 11:03:00 -0500

    Make basic loading and error ui indicators

--- src/App.tsx
+++ src/App.tsx
@@ -10,5 +10,26 @@ export default function App() {
   const dispatch = useAppDispatch();
   useFetchData(dispatch);
 
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <main className="flex flex-col items-center min-h-screen pt-20 text-white bg-gray-900">
+      <h1 className="mb-10 text-3xl font-bold tracking-tight">
+        dv01 Loan Analysis
+      </h1>
+
+      {error && (
+        <p className="py-10 text-2xl font-bold tracking-tight text-center">
+          There has been an error
+        </p>
+      )}
+      {loading ? (
+        <p className="py-10 text-2xl font-bold tracking-tight text-center">
+          Loading Data
+        </p>
+      ) : (
+        <>
+          <div className="space-y-6">{/* Visualization */}</div>
+        </>
+      )}
+    </main>
+  );
 }
